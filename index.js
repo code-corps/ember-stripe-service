25bfa7a550d45c02cf432760642906eb37c12969
Juan Pablo Buritica <buritica@gmail.com>
2015-01-26 00:57:28 -0500

    [add] inject stripe.js script in body

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'stripe-service'
+  name: 'stripe-service',
+  contentFor: function(type) {
+    // we use body since Stripe must be available before
+    if (type === 'body') {
+      return '<script type="text/javascript" src="https://js.stripe.com/v2/"></script>';
+    }
+  }
 };
